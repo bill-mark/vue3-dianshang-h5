4c14fa3d00f54711c0f304caed9d0d9827d49582
bill.mark <2286449127@qq.com>
2021-11-08 17:20:42 +0800

    增加登录页

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,27 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '../views/home/Home'
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import( '../views/login/Login')
+  }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+router.beforeEach((to, from ,next) => {
+  const { isLogin } = localStorage;
+  (isLogin || to.name === "Login") ? next() : next({ name: 'Login'});
 })
 
 export default router
---
+++ src/views/login/Login.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="wrapper">
+
+        <div class="wrapper_input">
+            <input class="wrapper_input_content" placeholder="手机号">
+        </div>
+
+        <div class="wrapper_input">
+            <input class="wrapper_input_content" placeholder="密码" type="password">
+        </div>
+   
+        <div class="wrapper_login-button" @click="handleLogin">
+            登陆
+        </div>
+
+        <div class="wrapper_login-link">
+            立即注册
+        </div>
+
+    </div>
+</template>
+
+<script>
+import {useRouter} from 'vue-router'
+export default {
+    name:'login',
+    setup(){
+        const router = useRouter()
+        const handleLogin = ()=>{
+            localStorage.isLogin = true
+            router.push({name:'Home'})
+        }
+        return {handleLogin}
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
