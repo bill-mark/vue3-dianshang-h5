a46323a818fd0510fc3e9096efce67d1a280bdfc
bill.mark <2286449127@qq.com>
2021-11-10 16:47:02 +0800

    setup规整逻辑流之后

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -18,53 +18,59 @@
     </div>
 
     <div class="wrapper_login-button" @click="handleLogin">登陆</div>
-    <div class="wrapper_login-link">立即注册</div>
+
     <Toast v-if="show" :message="toastMessage" />
   </div>
 </template>
 
 <script>
-import { reactive,toRefs } from "vue";
+import { reactive, toRefs } from "vue";
 import { useRouter } from "vue-router";
 import { login } from "@/api/login";
 import Toast, { useToastEffect } from "@/components/Toast";
+
+//处理登陆逻辑
+const useLoginEffect = (showToast) => {
+  const router = useRouter();
+  const userdata = reactive({
+    username: "",
+    password: "",
+  });
+
+  const handleLogin = async () => {
+    let { data } = await login({
+      username: userdata.username,
+      password: userdata.password,
+    });
+
+    if (data.code === 0) {
+      localStorage.isLogin = true;
+      router.push({ name: "Home" });
+    } else {
+      showToast("登陆失败");
+    }
+  };
+  
+  const {username,password} = toRefs(userdata)
+  return {username,password,handleLogin}
+};
+
 export default {
   name: "login",
   components: {
     Toast,
   },
-   // setup 只控制代码流程
+  // setup 只控制代码流程
   setup() {
-    const router = useRouter();
-    const userdata = reactive({
-      username: "",
-      password: "",
-    });
-    const { show,toastMessage, showToast } = useToastEffect();
+    const { show, toastMessage, showToast } = useToastEffect();
+    const {username, password,handleLogin} = useLoginEffect(showToast)
 
-    const handleLogin = async () => {
-      let { data } = await login({
-        username: userdata.username,
-        password: userdata.password,
-      });
-
-      if (data.code === 0) {
-        localStorage.isLogin = true;
-        router.push({ name: "Home" });
-      } else {
-        showToast("登陆失败");
-      }
+    return { 
+        username, password,handleLogin,show, toastMessage 
     };
-
-    const {username,password} = toRefs(userdata)
-    return { username,password,handleLogin, show,toastMessage };
   },
-}
+};
 
-//处理登陆逻辑
-  const useLoginEffect = ()=>{
-      
-  }
 
 </script>
 
